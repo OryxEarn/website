f7cf764e23222ac8b7b0869e3bf4737c15d138a3
maxlaxs <latityde314@gmail.com>
2018-12-22 18:09:41 +0100

    Added wisper, how many online, nicknames. Fixed css and javascrip
    Added wisper, how many online, nicknames. Fixed css and javascrip

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-var app = require('express')();
+var express = require("express"),
+    app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+  app.use(express.static('public'));
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+	socket.on('message', function(msg, id){
+		if (msg.slice(0, 3) == "/w ") {
+			wid = msg.split(" ")[1];
+			msg = msg.split(" ").slice(2).join(" ");
+			io.to(`${wid}`).emit('whisper', msg, wid, id);
+			io.to(`${socket.id}`).emit('whisper', msg, wid, id);
+		} else {
+			io.emit('message', msg, id);
+		}
+	});
+	var online = 0;
+	for (let i in io.sockets.clients().sockets) {
+		online ++;
+	}
+	io.emit("online", online);
+	socket.on('disconnect', function(){
+		var online = 0;
+		for (let i in io.sockets.clients().sockets) {
+			online ++;
+		}
+		io.emit("online", online);
+	});
 });
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
---
+++ public/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	var socket = io();
+	$('#sendmsg').click(function(){
+		//$('#m').val("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		var msg = $('#m').val();
+		socket.emit('message', msg, socket.id);
+		$('#m').val('');
+		return false;
+	});
+	socket.on('message', function(msg, id){
+		/*if( ($("#chat-window")[0].scrollHeight - $("#chat-window")[0].scrollTop - $("#chat-window")[0].clientHeight) < 200){
+			$("#chat-window").animate({ scrollTop: $('#chat-window')[0].scrollHeight}, 400);
+		}*/
+		var html = "<li class='chatmsg'>" + id + ":" + "<br>" + msg + "</li>";
+		$('#messages').append(html);
+	});
+	socket.on('whisper', function(msg, wid, id){
+		/*if( ($("#chat-window")[0].scrollHeight - $("#chat-window")[0].scrollTop - $("#chat-window")[0].clientHeight) < 200){
+			$("#chat-window").animate({ scrollTop: $('#chat-window')[0].scrollHeight}, 400);
+		}*/
+		var html;
+		if (socket.id == id) {
+			var html = "<li class='chatmsg'>You whisper to " + wid + ":" + "<br>" + msg + "</li>";
+		} else {
+			var html = "<li class='chatmsg'>" + id + " whispers to you:" + "<br>" + msg + "</li>";
+		}
+		$('#messages').append(html);
+	});
+	socket.on("online", function(online) {
+		document.getElementById("online").innerText = "Online: " + online;
+	})
+});
