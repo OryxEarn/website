446fcdcc22eb856c388a07bb99db99d43776db98
maxlaxs <latityde314@gmail.com>
2019-01-03 13:22:10 +0100

    Fixed nameing4.0

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var port = process.env.PORT || 3000;
-var num = 0;
+num = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
